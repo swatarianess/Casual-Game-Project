14866d0136f3b80a7c7cd28c627151aead14343d
Steve <swatarianess@gmail.com>
2015-11-30 18:57:22 +0100

    Movement and drawing finished. Draw whole array not working however.

--- TestFolder/Tal/ball.js
+++ TestFolder/Tal/ball.js
@@ -4,6 +4,9 @@ function Ball (radius, color) {
   this.x = 0;
   this.y = 0;
   this.radius = radius;
+  this.vx = 0;
+  this.vy = 0;
+  this.mass = 0;
   this.rotation = 0;
   this.scaleX = 0.4;
   this.scaleY = 0.4;
@@ -16,7 +19,7 @@ Ball.prototype.draw = function (context) {
   context.translate(this.x, this.y);
   context.rotate(this.rotation);
   context.scale(this.scaleX, this.scaleY);
-  
+
   context.lineWidth = this.lineWidth;
   context.fillStyle = this.color;
   context.beginPath();
@@ -30,21 +33,11 @@ Ball.prototype.draw = function (context) {
   context.restore();
 };
 
-Ball1.prototype.draw = function (context) {
-  context.save();
-  context.translate(this.x, this.y);
-  context.rotate(this.rotation);
-  context.scale(this.scaleX, this.scaleY);
-  
-  context.lineWidth = this.lineWidth;
-  context.fillStyle = this.color;
-  context.beginPath();
-  //x, y, radius, start_angle, end_angle, anti-clockwise
-  context.arc(0, 0, this.radius, 0, (Math.PI * 2), true);
-  context.closePath();
-  context.fill();
-  if (this.lineWidth > 0) {
-    context.stroke();
-  }
-  context.restore();
+Ball.prototype.getBounds = function () {
+  return {
+    x: this.x - this.radius,
+    y: this.y - this.radius,
+    width: this.radius * 2,
+    height: this.radius * 2
+  };
 };
